51f323ec805d5ef714db97733dafb7747121f9a2
pipo <ccampos.cartes@gmail.com>
2018-12-18 00:45:32 -0300

    test

---
+++ src/components/Header.vue
@@ -0,0 +1,83 @@
+<template>
+    <nav class="navbar is-light is-fixed-top" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Carga horaria
+      </a>
+
+      <a class="navbar-item">
+        Agregar datos
+      </a>
+
+      <a class="navbar-item">
+        Generar informe
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Opciones
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+
+       <Selector />
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+import Selector from './Selector.vue'
+
+export default {
+    name: 'Header',
+    components: {
+        Selector,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Selector.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+    <b-field>
+        <b-radio-button v-model="radioButton"
+            native-value="Nope"
+            type="is-primary">
+            <b-icon icon="check"></b-icon>
+            <span>Horas lectivas</span>
+        </b-radio-button>
+
+        <b-radio-button v-model="radioButton"
+            native-value="Yep"
+            type="is-primary">
+            <b-icon icon="check"></b-icon>
+            <span>Horas no lectivas</span>
+        </b-radio-button>
+    </b-field>
+
+</section>
+</template>
+
+<script>
+export default {
+    name: 'Selector',
+    data() {
+        return {
+            radioButton: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
