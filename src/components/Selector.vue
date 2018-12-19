2259a74ea2add30d27bb5e05d35914164066d66d
pipo <ccampos.cartes@gmail.com>
2018-12-18 23:37:53 -0300

    select

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,35 +1,33 @@
 <template>
-    <section>
+  <div class="navbar-item">
     <b-field>
-        <b-radio-button v-model="radioButton"
-            native-value="Nope"
-            type="is-primary">
-            <b-icon icon="check"></b-icon>
-            <span>Horas lectivas</span>
-        </b-radio-button>
+      <b-radio-button v-model="radioButton" native-value="lectivas" type="is-primary">
+        <span class="text">Horas lectivas</span>
+        <b-icon icon="book-open"></b-icon>
+      </b-radio-button>
 
-        <b-radio-button v-model="radioButton"
-            native-value="Yep"
-            type="is-primary">
-            <b-icon icon="check"></b-icon>
-            <span>Horas no lectivas</span>
-        </b-radio-button>
+      <b-radio-button v-model="radioButton" native-value="no-lectivas" type="is-primary">
+        <span class="text">Horas no lectivas</span>
+        <b-icon icon="book"></b-icon>
+      </b-radio-button>
     </b-field>
-
-</section>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Selector',
-    data() {
-        return {
-            radioButton: ''
-        }
-    }
-}
+  name: "Selector",
+  data() {
+    return {
+      radioButton: "lectivas"
+    };
+  }
+};
 </script>
 
 <style>
-
+.text {
+  margin-right: 5px;
+}
 </style>
+
